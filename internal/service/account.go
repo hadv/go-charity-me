9d9ec3ddcf0b23a92d356754d71bb5a5e916b9d5
hadv <dvietha@gmail.com>
2019-04-29 14:43:05 +0700

    use sony gobreaker to implement circuit breaker

--- internal/service/account.go
+++ internal/service/account.go
@@ -15,16 +15,19 @@ type AccountService interface {
 	Register(ctx context.Context, user *model.User) (*model.User, error)
 }
 
+// Account service
 type Account struct {
 	repo repo.UserRepo
 }
 
+// NewAccount create new account service
 func NewAccount(repo repo.UserRepo) *Account {
 	return &Account{
 		repo: repo,
 	}
 }
 
+// Login check account login and generate user token
 func (a *Account) Login(ctx context.Context, email, password string) (*model.User, error) {
 	user, err := a.repo.FindByEmail(ctx, email)
 	if err != nil {
@@ -38,12 +41,14 @@ func (a *Account) Login(ctx context.Context, email, password string) (*model.Use
 		return nil, err
 	}
 	user.Token = token
-	if err = a.repo.Update(ctx, user); err != nil {
+	usr, err := a.repo.Update(ctx, user)
+	if err != nil {
 		return nil, err
 	}
-	return user, nil
+	return usr, nil
 }
 
+// Register create new account
 func (a *Account) Register(ctx context.Context, user *model.User) (*model.User, error) {
 	if user.Password != user.ConfirmPassword {
 		return nil, errors.New("password and confirm password are not matched")
@@ -56,12 +61,10 @@ func (a *Account) Register(ctx context.Context, user *model.User) (*model.User, 
 		Password:  hashAndSalt([]byte(user.Password)),
 	}
 
-	if err := a.repo.Create(ctx, usr); err != nil {
+	user, err := a.repo.Create(ctx, usr)
+	if err != nil {
 		return nil, errors.Wrap(err, "cannot register new user")
 	}
 
-	if usr, err := a.repo.Get(ctx, usr.ID); err != nil {
-		return nil, errors.Wrapf(err, "cannot get user by id=%s", usr.ID)
-	}
-	return usr, nil
+	return user, nil
 }
